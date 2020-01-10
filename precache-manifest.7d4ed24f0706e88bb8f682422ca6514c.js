5dea85ae8c2c7d5b92a73ad7b713bc5ab3edc8f3
Chris Heyrendt <cheyrendt@outlook.com>
2020-01-10 14:49:00 +0100

    Updates

---
+++ precache-manifest.7d4ed24f0706e88bb8f682422ca6514c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b4ed0b1d0213a1547980984d68537f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2a5113470b4403fcf512",
+    "url": "/monsters-rolodex/static/css/main.6133ee5d.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a5113470b4403fcf512",
+    "url": "/monsters-rolodex/static/js/main.194b8312.chunk.js"
+  },
+  {
+    "revision": "d90ac9b9402da2dda087",
+    "url": "/monsters-rolodex/static/js/runtime-main.f8e4036a.js"
+  }
+]);
